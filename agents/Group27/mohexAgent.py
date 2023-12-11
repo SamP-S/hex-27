99b0c675fef235c93c9ed5a63e6af7615a6f1eb0
edawson650 <ethan.dawson@student.manchester.ac.uk>
2023-12-11 13:11:04 +0000

    better mohex

--- agents/Group27/mohexAgent.py
+++ agents/Group27/mohexAgent.py
@@ -3,7 +3,10 @@ from random import choice
 from time import sleep
 import subprocess
 import time
-RESPONSES = []
+import sys
+import select
+sys.stdout = open('output', 'w')
+MOVES =[]
 # Blue & White connect horizontally
 # Red & Black vertically
 mohex_colour_map = {'R': 'Bl', 'Bl': 'R', 'B': 'W', 'W': 'B'}
@@ -12,13 +15,13 @@ mohex_colour_map = {'R': 'Bl', 'Bl': 'R', 'B': 'W', 'W': 'B'}
 
 
 class mohexAgent():
-    """This class describes the default Hex agent. It will randomly send a
-    valid move at each turn, and it will choose to swap with a 50% chance.
+    """This class describes an agent that interfaces with the MoHex
+    executable. It is a finite-state machine that cycles through
+    waiting for input and sending moves.
     """
 
     HOST = "127.0.0.1"
     PORT = 1234
-    # Create dictionaries to convert between letters and integers
 
     def __init__(self):
         # Start the subprocess when the agent is initialized
@@ -27,14 +30,17 @@ class mohexAgent():
     def _start_subprocess(self):
         # Replace 'your_c_agent_executable' with the actual path to your C agent executable
         self._process = subprocess.Popen(["./agents/Group27/mohex"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,  
-                                         #stderr=subprocess.DEVNULL, 
+                                         stderr=subprocess.DEVNULL, 
                                          text=True)
         time.sleep(1)
+        self._send_mohex_command("boardsize 11 11")
+        response = self._read_mohex_response()
         print("Game start")
 
     def _terminate_subprocess(self):
         # Terminate the subprocess when the agent is closed
         self._process.terminate()
+        #print("Victory!")
 
     def run(self):
         """A finite-state machine that cycles through waiting for input
@@ -47,7 +53,7 @@ class mohexAgent():
         self._turn_count = 1
         self._choices = []
         self._opponent_moves = []
-        self._swapped = True
+        self._opp_swapped = False
         
         states = {
             1: mohexAgent._connect,
@@ -92,49 +98,28 @@ class mohexAgent():
                 return 4
 
         else:
-            print("ERROR: No START message received.")
+            # print("ERROR: No START message received.")
             return 0
 
     def _make_move(self):
         """Updates mohex board and requests it to generate move
         Then plays move
         """
-        print(f"Colour: {self._colour}")
-        if (self._turn_count == 2 and choice([0, 1]) == 1):
+        # print(f"Colour: {self._colour}")
+        if (self._turn_count == 1):
+            # play first move
+            mohex_colour = mohex_colour_map[self._colour][0]
+            self._send_mohex_command(f"play {mohex_colour} g7")
+            response = self._read_mohex_response()
+            move = f"{6},{6}\n"
+            self._s.sendall(bytes(move, "utf-8"))
+            MOVES.append([self._colour, move])
+            print("DEBUG: Bot move: ", move)
+        elif (self._turn_count == 2 and choice([0, 1]) == 1):
             msg = "SWAP\n"
+            self._s.sendall(bytes(msg, "utf-8"))
         else:
-            if self._turn_count == 1:
-                self._send_mohex_command("boardsize 11 11")
-                response = self._read_mohex_response()
-                mohex_colour = mohex_colour_map[self._colour]
-                self._send_mohex_command(f"genmove {mohex_colour[0]}")
-                response = self._read_mohex_response()
-                
-                move = self.mohex_to_hex_board(response[2], int(response[3]))
-                msg = f"{move[0]},{move[1]}\n"
-            else:
-                # Send opponent's most recent move to mohex
-                if self._turn_count != 3:
-                    opp_move = self._opponent_moves[-1]
-                    
-                    opp_move = self.hex_to_mohex_board(opp_move[0], str(opp_move[1]))
-
-                    opp_mohex_colour = mohex_colour_map[self.opp_colour()][0]
-                    self._send_mohex_command(f"play {opp_mohex_colour} {opp_move[0]+str(opp_move[1])}")
-                    response = self._read_mohex_response()
-
-                # Request move from mohex
-                mohex_colour = mohex_colour_map[self._colour][0]
-                self._send_mohex_command(f"genmove {mohex_colour}")
-                response = self._read_mohex_response()
-
-                # Send move to server
-                #move = self._convert_mohex_coordinates(response[2], int(response[3]))
-                move = self.mohex_to_hex_board(response[2], int(response[3]))
-                print(f"Move: {move}")
-                msg = f"{move[0]},{move[1]}\n"
-        
-        self._s.sendall(bytes(msg, "utf-8"))
+            self.play_round()
 
         return 4
     
@@ -149,15 +134,13 @@ class mohexAgent():
         else:
             if (data[1] == "SWAP"):
                 self._colour = self.opp_colour()
+                self._opp_swapped = True # Check if opponent used swap move
+                print("DEBUG: Opponent swapped")
             else:
                 x, y = data[1].split(",")
                 move = (int(x), int(y))
                 self._choices.remove(move)
                 self._opponent_moves.append(move)  # Store opponent's move
-
-                # Print the most recent opponent's move
-                # mohex_coords = self._(move[0], move[1])
-                # print(f"Opponent's most recent move: {mohex_coords[0]+mohex_coords[1]}")
 
             if (data[-1] == self._colour):
                 return 3
@@ -170,6 +153,7 @@ class mohexAgent():
         
         # Terminate the subprocess
         self._terminate_subprocess()
+        print(MOVES)
 
         return 0
 
@@ -186,22 +170,38 @@ class mohexAgent():
             return "None"
     
     def _send_mohex_command(self, command):
-        self._process.stdin.write(command + '\n')
-        self._process.stdin.flush()
+        if self._process.poll() is None:  # Check if the process is still running
+            self._process.stdin.write(command + '\n')
+            self._process.stdin.flush()
+        else:
+            raise Exception("Cannot send command: The process has terminated")
         print(f"Command sent: {command}")
 
-    def _read_mohex_response(self):
-        response = ''
-        while not response:
-            output = self._process.stdout.readline().strip()
-            if output == '' or output.endswith('\n'):
-                continue  # Skip empty or incomplete lines
-            response = output 
+    # def _read_mohex_response(self):
+    #     response = ''
+    #     while not response:
+    #         output = self._process.stdout.readline().strip()
+    #         if output == '' or output.endswith('\n'):
+    #             continue  # Skip empty or incomplete lines
+    #         response = output 
+    #     # print(f"RESPONSE: {response}")
+    #     return response
+    def _read_mohex_response(self, timeout=15.0):
+        response = 'timeout'
+        end_time = time.time() + timeout
+        while time.time() < end_time:
+            ready, _, _ = select.select([self._process.stdout], [], [], end_time - time.time())
+            if ready:
+                output = self._process.stdout.readline().strip()
+                if output == '' or output.endswith('\n'):
+                    continue  # Skip empty or incomplete lines
+                response = output
+                break
         print(f"RESPONSE: {response}")
         return response
     
     def mohex_to_hex_board(self, val1, val2):
-        firstval = val2-1
+        firstval = int(val2)-1
         secondval = ord(val1)-ord('a')
         return firstval, secondval
     
@@ -210,9 +210,45 @@ class mohexAgent():
         secondval = val1+1
         return firstval, secondval
     
-
-
-
+    def separate_letter_and_number(self, input_str):
+        letter = ''.join(char for char in input_str if char.isalpha())
+        number = ''.join(char for char in input_str if char.isdigit())
+        return letter, number
+    
+    def play_round(self):
+        """Play a round of Hex."""
+        # First we need to play opponents last move on MoHex
+        # Check if opponent swapped or not
+        if self._opp_swapped:
+            self._opp_swapped = False
+        else:
+            # Play opponents last move
+            opp_move = self._opponent_moves[-1]
+            MOVES.append([self.opp_colour(), opp_move])
+            opp_move = self.hex_to_mohex_board(opp_move[0], str(opp_move[1]))
+            opp_mohex_colour = mohex_colour_map[self.opp_colour()][0]
+            self._send_mohex_command(f"play {opp_mohex_colour} {opp_move[0]+str(opp_move[1])}")
+            response = self._read_mohex_response()
+            print(f"DEBUG: Opponent move: {self._opponent_moves[-1]}")
+        
+        # Then we need to generate our move
+        mohex_colour = mohex_colour_map[self._colour][0]
+        self._send_mohex_command(f"genmove {mohex_colour}")
+        response = self._read_mohex_response()
+        print("Response: ", response)
+        if response == 'timeout':
+            print("Timeout")
+            sys.exit()
+        
+        # Send move to server
+        print(f"RESPONSE: {response}")
+        move = self.separate_letter_and_number(response)
+        move = self.mohex_to_hex_board(move[0], move[1])
+        msg = f"{move[0]},{move[1]}\n"
+        # Then we need to play our move
+        MOVES.append([self._colour, msg])
+        print("DEBUG: Bot move: ", msg)
+        self._s.sendall(bytes(msg, "utf-8"))
 
 if (__name__ == "__main__"):
     agent = mohexAgent()
